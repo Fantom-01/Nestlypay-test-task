cdcf5f286fa509dd7cfe704d9b437da88cc1649a
Fantom <93797224+Fantom-01@users.noreply.github.com>
2023-03-06 16:22:07 +0100

    slider mobile review1

--- src/components/sliderItem.jsx
+++ src/components/sliderItem.jsx
@@ -5,10 +5,10 @@ const sliderItem = ({ heading, paragraph, image }) => {
   return (
     <div className="card">
         <main className="text">
-            <h3>{heading}</h3>
-            <p>{paragraph}</p>
+            <h3 className='slider--h3'>{heading}</h3>
+            <p className='slider--p'>{paragraph}</p>
         </main>
-        <img src={image} alt=""/>
+        <img src={image} alt="" className='slider--img'/>
     </div>
   )
 }
